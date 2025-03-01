8be1b14827b4a364b8820d72e4bc8a3d207f4bc5
SterlingDawn <vankiet14491@gmail.com>
2025-03-02 01:12:00 +0700

    fix otp veri

--- backend-app/src/main/java/utc/englishlearning/Encybara/service/OtpService.java
+++ backend-app/src/main/java/utc/englishlearning/Encybara/service/OtpService.java
@@ -11,32 +11,29 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class OtpService {
     private final Map<String, ReqOtpVerificationDTO> otpStorage = new ConcurrentHashMap<>();
-    private static final long OTP_EXPIRATION_TIME = 2 * 60 * 1000; // 1 phút
+    private static final long OTP_EXPIRATION_TIME = 2 * 60 * 1000; // 2 phút
 
     public String generateOtp(String email) {
         return String.valueOf((int) (Math.random() * 900000 + 100000));
     }
 
-
-
     public String saveRegisterData(String email, ResCreateUserDTO registerDTO, String otp, String type) {
-
-        String otpID= UUID.randomUUID().toString().replace("-", "").substring(0, 6).toUpperCase();
+        String otpID = UUID.randomUUID().toString().replace("-", "").substring(0, 6).toUpperCase();
         long tempTimestamp = System.currentTimeMillis();
 
-        ReqOtpVerificationDTO otpData= new ReqOtpVerificationDTO(otpID, otp, email, registerDTO, tempTimestamp, type);
+        ReqOtpVerificationDTO otpData = new ReqOtpVerificationDTO(otpID, otp, email, registerDTO, tempTimestamp, type);
         otpStorage.put(otpID, otpData);
         return otpID;
     }
 
-    public String updateOtp(String otpID){
+    public String updateOtp(String otpID) {
         ReqOtpVerificationDTO otpData = otpStorage.get(otpID);
-        if(otpData != null){
+        if (otpData != null) {
             otpData.setTimestamp(System.currentTimeMillis());
-            if(System.currentTimeMillis()- otpData.getTimestamp()<= OTP_EXPIRATION_TIME){
+            if (System.currentTimeMillis() - otpData.getTimestamp() <= OTP_EXPIRATION_TIME) {
                 return otpData.getOtp();
             }
-            String newOtp = generateOtp(otpID) ;
+            String newOtp = generateOtp(otpID);
             return newOtp;
         }
         return null;
@@ -44,24 +41,20 @@ public class OtpService {
 
     public ReqOtpVerificationDTO getOtpData(String otpID) {
         ReqOtpVerificationDTO otpData = otpStorage.get(otpID);
-        if(otpData == null) {
+        if (otpData == null) {
             return null;
         }
         long currentTime = System.currentTimeMillis();
-        if(currentTime - otpData.getTimestamp() > OTP_EXPIRATION_TIME) {
+        if (currentTime - otpData.getTimestamp() > OTP_EXPIRATION_TIME) {
             otpStorage.remove(otpID);
             return null;
         }
         return otpData;
     }
 
-
-    //    // Xác thực OTP
     public boolean validateOtp(String otpID, String otp) {
-
         ReqOtpVerificationDTO otpData = getOtpData(otpID);
-        System.out.println(otp+"  " + otpData.getOtp());
-        if( !otpData.getOtp().equals(otp)) {
+        if (otpData == null || !otpData.getOtp().equals(otp)) {
             return false; // otpData rỗng hoặc otp không đúng
         }
         return true;
