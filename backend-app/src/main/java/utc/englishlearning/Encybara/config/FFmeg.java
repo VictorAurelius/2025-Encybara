f8d084b999a3bbaeb1cf958252d0b25a85ee2bc4
tmd2311 <dung.kayc@gmail.com>
2025-04-13 14:00:10 +0700

    update feature/speech to text

--- backend-app/src/main/java/utc/englishlearning/Encybara/config/FFmeg.java
+++ backend-app/src/main/java/utc/englishlearning/Encybara/config/FFmeg.java
@@ -3,33 +3,36 @@ package utc.englishlearning.Encybara.config;
 import java.io.IOException;
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFprobe;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class FFmeg {
+
+  @Value("${ffmpeg.path}")
+  private String ffmpegPath;
+
+  @Value("${ffprobe.path}")
+  private String ffprobePath;
 
   @Bean
   public FFmpeg ffmpeg() {
     try {
-      FFmpeg fFmpeg =  new FFmpeg("F:\\ffmpeg-2025-03-31-git-35c091f4b7-essentials_build\\ffmpeg-2025-03-31-git-35c091f4b7-essentials_build\\bin\\ffmpeg.exe");
-      return fFmpeg;
+      return new FFmpeg(ffmpegPath);
     } catch (IOException e) {
-      throw new RuntimeException(e);
+      throw new RuntimeException("Error initializing FFmpeg", e);
     }
   }
 
   @Bean
   public FFprobe ffprobe() {
     try {
-      FFprobe ffprobe =  new FFprobe("F:\\ffmpeg-2025-03-31-git-35c091f4b7-essentials_build\\ffmpeg-2025-03-31-git-35c091f4b7-essentials_build\\bin\\ffprobe.exe");
-      return ffprobe;
+      return new FFprobe(ffprobePath);
     } catch (IOException e) {
-      throw new RuntimeException(e);
+      throw new RuntimeException("Error initializing FFprobe", e);
     }
   }
-
-
-
-
 }
+
+
+
