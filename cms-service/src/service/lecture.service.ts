66d831e4f2cb7670e050052a86cac0e0650cb21d
quyetvm183 <vuminhquyet18@gmail.com>
2025-12-13 23:59:37 +0700

    final

--- cms-service/src/service/lecture.service.ts
+++ cms-service/src/service/lecture.service.ts
@@ -122,8 +122,8 @@ class LectureService {
 
     try {
       const processedLink = materLink
-        .replace('http://0.0.0.0:8080', 'http://18.136.223.96:8080')  // ← Bỏ :8080
-        .replace('http://18.136.223.96:8080', 'http://18.136.223.96:8080')  // ← Bỏ :8080
+        .replace('http://0.0.0.0:8080', 'http://18.136.223.96')  // ← Bỏ :8080
+        .replace('http://18.136.223.96:8080', 'http://18.136.223.96')  // ← Bỏ :8080
         .replace(/ /g, '%20');
 
       console.log("Fetching from:", processedLink);
